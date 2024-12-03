fa3ad9d11f0b814fe4cf651d84a94bccfaca8d38
Gouki <{ID}+{username}@users.noreply.github.com>
2024-12-03 23:36:56 +0100

    Solution for day 3 part 1.

---
+++ day3/day3_part1/target/debug/day3_part1.d
@@ -0,0 +1,1 @@
+/home/gouki/Documents/Repos/adventofcode24/day3/day3_part1/target/debug/day3_part1: /home/gouki/Documents/Repos/adventofcode24/day3/day3_part1/src/main.rs
---
+++ day3/day3_part1/target/debug/deps/day3_part1-b6979b49c9799e8c.d
@@ -0,0 +1,5 @@
+/home/gouki/Documents/Repos/adventofcode24/day3/day3_part1/target/debug/deps/day3_part1-b6979b49c9799e8c: src/main.rs
+
+/home/gouki/Documents/Repos/adventofcode24/day3/day3_part1/target/debug/deps/day3_part1-b6979b49c9799e8c.d: src/main.rs
+
+src/main.rs:
